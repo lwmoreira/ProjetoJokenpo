15b8cc888f129bbb45429b237ef2ba290c6afedd
lwmoreira <lwmoreira01@gmail.com>
2023-09-28 14:03:54 -0300

    Jokenpô

---
+++ scripts.js
@@ -0,0 +1,53 @@
+const result = document.querySelector(".result")
+
+const humanScore = document.querySelector("#human-score")
+
+const machineScore = document.querySelector("#machine-score")
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) => {
+
+    playTheGame(humanChoice, playMachine())
+
+}
+
+const playMachine = () => {
+
+    const choices = ["rock", "paper", "scissors"]
+    const radomNumber = Math.floor(Math.random() * 3)
+    
+
+  
+
+    return choices[radomNumber]
+}
+
+const playTheGame = (human, machine) => {
+
+  
+
+    if (human === machine) {
+
+        result.innerHTML = "Deu empate"
+    } else if (
+        (human === "paper" && machine === "rock") ||
+        (human === "rock" && machine === "scissors") ||
+        (human === "scissors" && machine === "paper")) 
+        
+    {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+
+        result.innerHTML = "Voce ganhou!"
+    } else {
+
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = "Voce Perdeu!"
+
+    }
+
+}
+
